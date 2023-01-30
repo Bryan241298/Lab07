30e022a0103dd7cd0785a045a031fccf7229b708
Bryan <bryan_goh_@hotmail.com>
2023-01-30 15:49:27 +0800

    Completed Lab7

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
-
+    public Rigidbody rb;
+    public int height;
+    public int Score;
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * height;
             thisAnimation.Play();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "lose")
+        {
+            SceneManager.LoadScene("gamelose");
+        }
+
+        if (other.gameObject.tag == "Score")
+        {
+            Score++;
+            Destroy(other.gameObject);
+        }
     }
 }
